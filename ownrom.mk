5e3ec11e9eceef49f02a3da93cb4ba3955963f1e
maxprzemo <przemekbanasiak1@wp.pl>
2016-04-24 18:52:48 +0200

    OwnROM time :)

--- ownrom.mk
+++ ownrom.mk
@@ -12,16 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit some common OwnROM stuff.
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
+
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
-# You do not need to define. These values will be defined in vanir_osprey.mk
 ## Device identifier. This must come after all inclusions
-# PRODUCT_DEVICE := osprey
-# PRODUCT_NAME := cm_osprey
-# PRODUCT_BRAND := Motorola
-# PRODUCT_MANUFACTURER := Motorola
-# PRODUCT_RELEASE_NAME := osprey
+PRODUCT_DEVICE := osprey
+PRODUCT_NAME := ownrom_osprey
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := osprey
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
